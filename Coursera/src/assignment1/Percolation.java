092a9e90842be2b33c6c54b9d27f32296c8aadde
cdonthini <cdonthini@gmail.com>
2013-04-10 13:37:28 -0400

    Initial Commit

---
+++ Coursera/src/assignment1/Percolation.java
@@ -0,0 +1,40 @@
+package assignment1;
+
+public class Percolation {
+	final int BLOCK = 0;
+	final int OPEN = 1;
+	final int FULL = 2;
+	int grid[][];
+	WeightedQuickUnionUF unionFind;
+	
+	public Percolation( int N )              // create N-by-N grid, with all sites blocked
+    {
+		grid = new int [N][N] ;
+		unionFind = new WeightedQuickUnionUF (N);
+    }
+	
+	public void open(int i, int j)         // open site (row i, column j) if it is not already
+	{
+		grid[i][j] = OPEN;
+	}
+	
+	public boolean isOpen(int i, int j)    // is site (row i, column j) open?
+	{
+		if ( grid[i][j] == OPEN ) return true;
+		else return false;
+		   
+	}
+	
+	public boolean isFull(int i, int j)    // is site (row i, column j) full?
+	{
+		if ( grid[i][j] == FULL ) return true;
+		else return false;
+	}
+	
+	public boolean percolates()            // does the system percolate?
+	{
+		unionFind.count(); //TODO 
+		return false;
+		   
+	}
+}
