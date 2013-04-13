b0743c26748c626c2b312920cb285643e83473f4
cdonthini <cdonthini@gmail.com>
2013-04-13 01:36:40 -0400

    ADDED & REMOVED ANYTHINGS! NEW PLAN :)

--- Coursera/src/assignment1/Percolation.java
+++ Coursera/src/assignment1/Percolation.java
@@ -2,103 +2,99 @@ package assignment1;
 
 public class Percolation {
 	int size = 0;
-	boolean startFill = false;
-	boolean percs = false;
+	boolean FRowOn = false;
 	final int BLOCK = 0;
 	final int OPEN = 1;
 	final int FULL = 2;
+	final int UP = 1;
+	final int DOWN = 2;
+	final int RIGHT = 3;
+	final int LEFT = 4;
+	
 	int grid[][];
 	WeightedQuickUnionUF quickUnion;
-	
-	public Percolation( int N )              // create N-by-N grid, with all sites blocked
-    {
+
+	public Percolation(int N) // create N-by-N grid, with all sites blocked
+	{
 		size = N;
-		grid = new int [N][N] ;
-		quickUnion = new WeightedQuickUnionUF ( N * N );
-    }
-	
-	public void open(int i, int j)         // open site (row i, column j) if it is not already
-	{
-		
-		
-		if ( i == 0 ) startFill = true;
-		
-		if ( i == 0 ) {
-			
-			grid[i][j] = FULL;
-			if ( i == size - 1 ) percs = true;
-			if ( ( i - 1 ) >= 0 && isOpen( i - 1 , j ) ) {
-				quickUnion.union( ( size * ( i - 1 ) ) + j , size * ( i )  + j); // up
-				fill ( i - 1 , j );
-			}
-			if ( ( j - 1 ) >= 0 && isOpen( i , j - 1 ) ) {
-				quickUnion.union( ( size * ( i ) ) + ( j - 1) , size * ( i )  + j); // left 
-				fill ( i , j - 1 );
-			}
-			if ( ( j + 1 ) < size && isOpen( i , j + 1 ) ) {
-				quickUnion.union( ( size * ( i ) ) + ( j + 1) , size * ( i )  + j); // right
-				fill( i , j + 1 );
-			}
-			if ( ( i + 1 ) < size && isOpen( i + 1 , j ) ) {
-				quickUnion.union( ( size * ( i + 1 ) ) + j , size * ( i )  + j); // down 
-				fill( i + 1 , j );
-			}
-		}
-		else {
-			grid[i][j] = OPEN;
-			
-			if ( ( i - 1 ) >= 0 && isOpen( i - 1 , j ) ) {
-				quickUnion.union( ( size * ( i - 1 ) ) + j , size * ( i )  + j); // up
-			}
-			if ( ( j - 1 ) >= 0 && isOpen( i , j - 1 ) ) {
-				quickUnion.union( ( size * ( i ) ) + ( j - 1) , size * ( i )  + j); // left 
-			}
-			if ( ( j + 1 ) < size && isOpen( i , j + 1 ) ) {
-				quickUnion.union( ( size * ( i ) ) + ( j + 1) , size * ( i )  + j); // right
-			}
-			if ( ( i + 1 ) < size && isOpen( i + 1 , j ) ) {
-				quickUnion.union( ( size * ( i + 1 ) ) + j , size * ( i )  + j); // down 
-			}
-		}
+		grid = new int[N][N];
+		quickUnion = new WeightedQuickUnionUF(N * N);
 	}
-	
-	private void fill ( int i , int j ) {
+
+	public void open(int i, int j) // open site (row i, column j) if it is not
+									// already
+	{
+		if ( i == 0 ) FRowOn = true;
+		grid[i][j] = OPEN;
+		if ((i - 1) >= 0 && isOpen(i - 1, j)) {
+			quickUnion.union((size * (i - 1)) + j, size * (i) + j); // up
+			
+		}
+		if ((j - 1) >= 0 && isOpen(i, j - 1)) {
+			quickUnion.union((size * (i)) + (j - 1), size * (i) + j); // left
+			
+		}
+		if ((j + 1) < size && isOpen(i, j + 1)) {
+			quickUnion.union((size * (i)) + (j + 1), size * (i) + j); // right
+			
+		}
+		if ((i + 1) < size && isOpen(i + 1, j)) {
+			quickUnion.union((size * (i + 1)) + j, size * (i) + j); // down
+			
+		}
+
+	}
+
+	private void fill(int i, int j , int prev ) {
+		
+		
 		grid[i][j] = FULL;
-		if ( i == size - 1 ) percs = true;
-		if ( ( i - 1 ) >= 0 && isOpen( i - 1 , j ) ) {
-			quickUnion.union( ( size * ( i - 1 ) ) + j , size * ( i )  + j); // up
-			fill ( i - 1 , j );
-		}
-		if ( ( j - 1 ) >= 0 && isOpen( i , j - 1 ) ) {
-			quickUnion.union( ( size * ( i ) ) + ( j - 1) , size * ( i )  + j); // left 
-			fill ( i , j - 1 );
-		}
-		if ( ( j + 1 ) < size && isOpen( i , j + 1 ) ) {
-			quickUnion.union( ( size * ( i ) ) + ( j + 1) , size * ( i )  + j); // right
-			fill( i , j + 1 );
-		}
-		if ( ( i + 1 ) < size && isOpen( i + 1 , j ) ) {
-			quickUnion.union( ( size * ( i + 1 ) ) + j , size * ( i )  + j); // down 
-			fill( i + 1 , j );
-		}
-	}
-	public boolean isOpen(int i, int j)    // is site (row i, column j) open?
-	{
-		if ( grid[i][j] == OPEN ) return true;
-		else return false;
-		   
-	}
-	
-	public boolean isFull(int i, int j)    // is site (row i, column j) full?
-	{
-		if ( grid[i][j] == FULL ) return true;
-		else return false;
-	}
-	
-	public boolean percolates()            // does the system percolate?
-	{
 		
-		return percs;
-		   
+		if ((i - 1) >= 0 && isFull(i - 1, j) && ( prev != DOWN ) ) {
+			//quickUnion.union((size * (i - 1)) + j, size * (i) + j); // up
+			fill(i - 1, j, UP);
+		}
+		if ((j - 1) >= 0 && isFull(i, j - 1)  && ( prev != RIGHT ) ) {
+			//quickUnion.union((size * (i)) + (j - 1), size * (i) + j); // left
+			fill(i, j - 1, LEFT);
+		}
+		if ((j + 1) < size && isFull(i, j + 1) && ( prev != LEFT ) ) {
+			//quickUnion.union((size * (i)) + (j + 1), size * (i) + j); // right
+			fill(i, j + 1, RIGHT);
+		}
+		if ((i + 1) < size && isFull(i + 1, j)  && ( prev != UP ) ) {
+			//quickUnion.union((size * (i + 1)) + j, size * (i) + j); // down
+			fill(i + 1, j, DOWN);
+		}
+	}
+
+	public boolean isOpen(int i, int j) // is site (row i, column j) open?
+	{
+		if (grid[i][j] == OPEN)
+			return true;
+		else
+			return false;
+
+	}
+
+	public boolean isFull(int i, int j) // is site (row i, column j) full?
+	{
+		if (grid[i][j] >= OPEN)
+			return true;
+		else
+			return false;
+	}
+
+	public boolean percolates() // does the system percolate?
+	{
+		if( FRowOn ) {
+			for ( int i = 0; i < size; i++ ) {
+				if ( grid[0][i] >= OPEN) {
+					fill( 0 , i , 0);
+				}
+			}
+		}
+		return false;
+
 	}
 }
