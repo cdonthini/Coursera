84445388ff7ba3db04d0f3c74642823417217dc2
cdonthini <cdonthini@gmail.com>
2013-04-16 00:33:33 -0400

    assignment 2 - Started

---
+++ Coursera/src/assignment2/Deque.java
@@ -0,0 +1,13 @@
+package assignment2;
+
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+	
+	public Iterator<Item> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
