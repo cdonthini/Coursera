1405341bada07fcd5c2a3255dd129d462c632e46
cdonthini@gmail.com <Chaitanya@10.0.0.14>
2013-04-25 01:00:04 -0400

    filled hashtable. need to remove collinear points.

--- Coursera/src/assignment3/Fast.java
+++ Coursera/src/assignment3/Fast.java
@@ -1,6 +1,8 @@
 package assignment3;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
 
 public class Fast {
 	public static void main ( String[] args ) {
@@ -19,13 +21,29 @@ public class Fast {
 			ptArr[pointer++].draw();
 		}
 		Point origin = ptArr[0];
-		double slope[] = new double[numPoints-1];
+		int slope;
+		Hashtable<Integer, ArrayList<Point>> table = new Hashtable<Integer, ArrayList<Point>>();
 		
 		for( int i = 1; i < numPoints; i++ ){
-			slope[i-1] = origin.slopeTo(ptArr[i]);
+			slope = (int) (100.0 * origin.slopeTo(ptArr[i]));
+			
+			if ( table.containsKey(slope) ) {
+				table.get(slope).add(ptArr[i]);
+			}
+			else {
+				table.put(slope, new ArrayList<Point>());
+				table.get(slope).add(ptArr[i]);
+			}
 		}
-		Arrays.sort(slope);
 		
+		Enumeration<ArrayList<Point>> enumeration = table.elements();
+		
+		while( enumeration.hasMoreElements() ) {
+			if ( enumeration.nextElement().size() >= 4 ){
+				
+			}
+			
+		}
 		
 	}
 }
