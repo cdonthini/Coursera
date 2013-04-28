3db84b1ca69aeefb12120c0062614ae3d2010908
cdonthini@gmail.com <Chaitanya@10.0.0.14>
2013-04-28 17:41:11 -0400

    finished assignment 3

--- Coursera/src/assignment3/Fast.java
+++ Coursera/src/assignment3/Fast.java
@@ -16,15 +16,25 @@ public class Fast {
 		Point[] ptArr = new Point[numPoints];
 		
 		while ( !input.isEmpty() ) {
-			ptArr[pointer] = new Point(input.readInt(),input.readInt());
+			ptArr[pointer++] = new Point(input.readInt(),input.readInt());
 			
-			ptArr[pointer++].draw();
+			//ptArr[pointer++].draw();
 		}
+		Fast findCollinearPts = new Fast ();
+		while(ptArr[1] != null ) {
+			
+			ptArr = findCollinearPts.fast(ptArr,ptArr.length,pointer);
+		}
+	}
+	
+	public Point[] fast (Point[] ptArr, int numPoints, int pointer){
 		Point origin = ptArr[0];
+		ptArr[0] = null;
 		int slope;
 		Hashtable<Integer, ArrayList<Point>> table = new Hashtable<Integer, ArrayList<Point>>();
 		
 		for( int i = 1; i < numPoints; i++ ){
+			if(ptArr[i] == null) continue;
 			slope = (int) (100.0 * origin.slopeTo(ptArr[i]));
 			
 			if ( table.containsKey(slope) ) {
@@ -32,18 +42,35 @@ public class Fast {
 			}
 			else {
 				table.put(slope, new ArrayList<Point>());
+				table.get(slope).add(origin);
 				table.get(slope).add(ptArr[i]);
 			}
+			ptArr[i] = null;
 		}
 		
 		Enumeration<ArrayList<Point>> enumeration = table.elements();
-		
+		ArrayList<Point> alPoint;
+		pointer = 0;
+		numPoints = 0;
 		while( enumeration.hasMoreElements() ) {
-			if ( enumeration.nextElement().size() >= 4 ){
-				
+			alPoint = enumeration.nextElement();
+			if ( alPoint.size() >= 4 ){
+				for(int i = 0; i < alPoint.size(); i++) {
+					
+					//origin.drawTo(alPoint.get(i));
+					StdOut.print(alPoint.get(i));
+				}				
+				StdOut.print("\n");
 			}
-			
+			else {
+				for(int i = 1; i < alPoint.size(); i++) {
+					ptArr[pointer++]= alPoint.get(i);
+					numPoints++;
+				}
+			}			
 		}
 		
+		return ptArr;
 	}
+
 }
